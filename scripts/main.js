bf05a3496673ea55f067b23e11a31691866c2c51
advornikova <advornikova@gk21.ru>
2019-02-11 15:02:08 +0300

    added animation for detail image

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
 var ESC_KEY = 27;
 
  function setDetails(imageUrl, titleText) {
@@ -51,7 +53,12 @@ function hideDetail() {
 
 function showDetails() {
   'use strict';
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function () {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
 }
 
 function addKeyPressHandler() {
